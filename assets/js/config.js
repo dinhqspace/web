b1f9024f9fa0eeb3d86896d3c0d6e72ced1eb118
Quy Dinh Trinh <98367950+lmao9323@users.noreply.github.com>
2023-10-30 20:52:02 +0700

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,43 @@
+const BADGES = [
+  {
+    name: "HypeSquad Brillance", // Message that will appear when hovering over the badge
+    image: "assets/badges/hypesquad_brillance.svg",
+  },
+  {
+    name: "Active Developer", // Message that will appear when hovering over the badge
+    image: "assets/badges/activedeveloperbadge.svg"
+  },
+  {
+    name: "Subscriber since Sep 20, 2019", // Message that appears when you hover over it
+    image: "assets/badges/nitro.svg",
+  },
+  {
+    name: "Server boosting Sep 21, 2019", // Message that appears when you hover over it
+    image: "assets/badges/boost24month.svg",
+  },
+  {
+    name: "Orginally known as BCF | dinhq#0001", // Message that appears when you hover over it
+    image: "assets/badges/pomelo.svg", // Icon (in .svg) of the badge is in the "assets/badges" you choose
+  },
+]
+
+const USER_ID = "934670328318267403"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/folody_3-min.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Do not disturb", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
